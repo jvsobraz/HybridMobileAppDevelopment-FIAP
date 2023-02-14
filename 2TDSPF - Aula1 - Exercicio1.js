7a5c4a69d0c049007298b7a621e33c9f445ba9c4
jvsobraz <jvsobraz@hotmail.com>
2023-02-13 22:09:23 -0300

    Exercício 1

---
+++ 2TDSPF - Aula1 - Exercicio1.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Text, View, StyleSheet} from 'react-native';
+
+function Paulista() {
+  return (
+  <View style={styles.centered}>
+    <Text style={styles.title}>João Vitor Santiago de Oliveira Braz</Text>
+    <Text style={styles.title}>FIAP</Text>
+    <Text style={styles.title}>Análise e Desenvolvimento de Sistemas</Text>
+    <Text style={styles.title}>2TDSPF</Text>
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    centered: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+})
+
+export default Paulista;
