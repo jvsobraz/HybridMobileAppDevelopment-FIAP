ab8bbfe3afc9cd598493bbf7ca19ac8aefdb4a22
jvsobraz <jvsobraz@hotmail.com>
2023-03-10 09:42:47 -0300

    React Component View

---
+++ ReactComponenteView.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StatusBar, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <View>
+        <View style={{backgroundColor: 'crimson', height: 200, width: 200}}></View>
+        <View style={{alignSelf: 'center', backgroundColor: 'cornflowerblue',
+         borderRadius: 100, height: 200, marginVertical: 20, width: 200}}></View>
+         <View style={{alignItems: 'center', backgroundColor: 'greenyellow',
+         alignSelf: 'flex-end', height: 200, justifyContent: 'center', width: 300}}>
+        <Text>Texto dentro de uma View</Text>
+        </View>
+      <StatusBar style="light"/>  
+      </View>
+    );
+  }
+}
