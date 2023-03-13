207f8b8d6e461e16d5458760e673ec09267920b1
jvsobraz <jvsobraz@hotmail.com>
2023-03-12 21:35:26 -0300

    ReactFlex

---
+++ ReactFlex.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StatusBar, View } from 'react-native';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <View>
+                <View style={{
+                    width: 50, height: 50, backgroundColor: 'powderblue'
+                }} />
+                <View style={{
+                    width: 100, height: 100, backgroundColor: 'skyblue'
+                }} />
+                <View style={{
+                    width: 150, height: 150, backgroundColor: 'steelblue'
+                }} />
+                <StatusBar style="auto" />
+            </View>
+        );
+    }
+}
