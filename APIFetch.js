c8fc015e5c88c23bec5e261f8bc72056a7e250f4
João Vitor Santiago de Oliveira Braz <jvsobraz@hotmail.com>
2023-03-15 13:41:56 -0300

    Arrumando a API Fetch

--- APIFetch.js
+++ APIFetch.js
@@ -1,31 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View, StyleSheet, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator, FlatList, StatusBar, Text, View } from 'react-native';
 
 export default function App() {
-  const [movies, setMovies] = useState([]);
-  console.log(movies);
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
   const getMovies = async () => {
     try {
-      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const response = await fetch('https://reactnative.dev/movies.json');
       const json = await response.json();
-      setMovies(json.movies);
+      setData(json.movies);
     } catch (error) {
-      console.log(error);
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
-  };
+  }
 
   useEffect(() => {
     getMovies();
   }, []);
 
   return (
-    <View>
-      <FlatList
-        data={movies}
-        renderItem={({ item: movie }) => {
-          return <Text>{movie.title}</Text>;
-        }}
-      />
+    <View styles={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator /> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }) => id}
+          renderItem={({ item }) => (
+            <Text>{item.title}, {item.releaseYear}</Text>
+          )}
+        />
+      )}
+      <StatusBar barStyle="default" />
     </View>
   );
 }
